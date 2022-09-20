04f0e5bba867de3e17752cf59318e2d71f11d09f
Nikita Zelenkov <zelenkov4462@gmail.com>
2022-09-20 20:26:50 +0700

    fix *page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
 
 const NotFound = () => {
-  return <div>This page doesnt`t exist</div>;
+  const navigate = useNavigate();
+  return (
+    <Wrapper>
+      This page doesnt`t exist
+      <Button
+        style={{ justifyContent: "center" }}
+        onClick={() => navigate("/")}
+      >
+        Go Home Page
+      </Button>
+    </Wrapper>
+  );
 };
 
 export default NotFound;
